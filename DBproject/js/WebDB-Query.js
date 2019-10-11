a78783a5dd8a1d2f6f5d01783ff7598a962018fe
thanawong <tomta555@hotmail.com>
2019-10-11 14:23:34 +0700

    query employees

--- DBproject/js/WebDB-Query.js
+++ DBproject/js/WebDB-Query.js
@@ -39,6 +39,42 @@ function showProducts(){
 // function AddtoCart(){
 
 //   }
-function prodTotalCal(){
-  
+
+function empQuery(){
+  var db = openDatabase('ClassicModelShop', '1.0', 'Classic model shop v.1', 2 * 1024 * 1024);
+  var enumber;
+  var fname;
+  var lname;
+  var exten;
+  var email;
+  var office;
+  var reportsto;
+  var title;
+  const tableBody = document.querySelector('#TableBody')
+  db.transaction(function (tx) {
+    tx.executeSql('SELECT * FROM employees', [], function (tx, results) {
+      var len = results.rows.length, i;
+      for (i = 0; i < len; i++) {
+        enumber = results.rows.item(i).employeeNumber
+        fname = results.rows.item(i).firstName
+        lname = results.rows.item(i).lastName
+        exten = results.rows.item(i).extension
+        email = results.rows.item(i).email
+        office = results.rows.item(i).officeCode
+        reportsto = results.rows.item(i).reportsTo
+        title = results.rows.item(i).jobTitle
+        tableBody.innerHTML += `
+        <tr>
+        <td>`+enumber+`</td>
+        <td>`+fname+`</td>
+        <td>`+lname+`</td>
+        <td>`+exten+`</td>
+        <td>`+email+`</td>
+        <td>`+office+`</td>
+        <td>`+reportsto+`</td>
+        <td>`+title+`</td>
+      </tr>`;
+      }
+    }, null);
+  });
 }
